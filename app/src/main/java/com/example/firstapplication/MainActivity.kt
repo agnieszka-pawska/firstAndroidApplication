7713e718789f598d97961d452c4e3b3a02c46500
Agnieszka Pawska <agnieszka.pawska@allegro.pl>
2021-06-15 10:02:18 +0200

    click counter on a button

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.firstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.counter_button)
+        val textView: TextView = findViewById(R.id.textView)
+        var counter = 0
+
+        button.setOnClickListener(View.OnClickListener {
+            counter++
+            textView.text = getString(R.string.counter, counter.toString())
+        })
     }
 }
