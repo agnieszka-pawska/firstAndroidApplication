72c3a912b54304ed20270771c883a83deda24776
Agnieszka Pawska <agnieszka.pawska@allegro.pl>
2021-06-17 10:47:50 +0200

    added userLisViewModel WIP

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var usersViewModel: UserListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,21 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val userAdapter = UserAdapter(
-            mutableListOf(
-                User("user-id-1", "user-name-1", "user-1-email"),
-                User("user-id-2", "user-name-2", "user-2-email"),
-                User("user-id-3", "user-name-3", "user-3-email"),
-                User("user-id-4", "user-name-4", "user-4-email"),
-                User("user-id-5", "user-name-5", "user-5-email")
-            )
-        )
         val recyclerView = binding.recyclerView
-        recyclerView.adapter = userAdapter
 
+        usersViewModel = UserListViewModel()
+        usersViewModel.users.observe(
+            this,
+            { updatedUserList ->
+                recyclerView.adapter = UserAdapter(updatedUserList.toMutableList())
+            }
+        )
+        actionOnNewUserButton()
+    }
+
+    private fun actionOnNewUserButton() {
         val newUserButton = binding.newUserButton
         newUserButton.setOnClickListener {
-            userAdapter.add(generateNewUser())
+            usersViewModel.add(generateNewUser())
         }
     }
 
